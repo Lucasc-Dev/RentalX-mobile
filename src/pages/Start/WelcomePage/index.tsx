96672e27a1ba15f6f1d74617fb6019922084ea02
Lucas César <lucasambrogi1@hotmail.com>
2020-10-05 10:48:04 -0300

    First commit!

---
+++ src/pages/Start/WelcomePage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const WelcomePage: React.FC = () => {
+  return (
+    <Container>
+      <Text>WelcomePage</Text>
+    </Container>
+  );
+};
+
+export default WelcomePage;
