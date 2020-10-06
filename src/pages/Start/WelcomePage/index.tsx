e0c9f336a8a6731a637e521f8965c00c8b99acc6
Lucas César <lucasambrogi1@hotmail.com>
2020-10-05 22:29:31 -0300

    Welcome Page done.

--- src/pages/Start/WelcomePage/index.tsx
+++ src/pages/Start/WelcomePage/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-import { Container } from './styles';
+import logo from '../../../assets/images/XLogo.png';
+
+import { 
+  Container, 
+  VerticalContainer,
+  Logo, 
+  TitleContainer,
+  Title, 
+  Subtitle,
+  ButtonsContainer,
+  HorizontalButtonsContainer,
+  Button,
+  ButtonText,
+  BackButton,
+  BackButtonText,
+} from './styles';
 
 const WelcomePage: React.FC = () => {
   return (
     <Container>
-      <Text>WelcomePage</Text>
+      <VerticalContainer>
+        <Logo source={logo} />
+
+        <TitleContainer>
+          <Title>Seja</Title>
+          <Title>Bem-Vindo</Title>
+
+          <Subtitle>O que você deseja fazer?</Subtitle>
+        </TitleContainer>
+
+        <ButtonsContainer>
+          <HorizontalButtonsContainer>
+            <Button style={{ backgroundColor: '#dc1637' }}>
+              <ButtonText>Login</ButtonText>
+            </Button>
+
+            <Button style={{ backgroundColor: '#29292e' }}>
+              <ButtonText>Cadastro</ButtonText>
+            </Button>
+          </HorizontalButtonsContainer>
+
+          <BackButton>
+            <BackButtonText>Voltar</BackButtonText>
+          </BackButton>
+        </ButtonsContainer>
+      </VerticalContainer>
     </Container>
   );
 };
--- src/pages/Start/WelcomePage/styles.ts
+++ src/pages/Start/WelcomePage/styles.ts
@@ -1,5 +1,78 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+    flex: 1;
+    background-color: #1b1b1f;
+`;
 
+export const VerticalContainer = styled.View`
+    flex: 1;
+    
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+export const Logo = styled.Image`
+    
+`;
+
+export const TitleContainer = styled.View`
+
+`;
+
+export const Title = styled.Text`
+    font-family: 'Archivo-SemiBold';
+    font-weight: 600;
+    line-height: 44px;
+    text-align: center;
+    font-size: 40px;
+    color: #f4f5f6;
+`;
+
+export const Subtitle = styled.Text`
+    padding-top: 16px;
+    font-family: 'Inter-Medium-sInt=0';
+    font-weight: 400;
+    text-align: center;
+    font-size: 16px;
+    color: #dedee3;
+`;
+
+export const ButtonsContainer = styled.View`
+    margin-top: 70px;
+`;
+
+export const HorizontalButtonsContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: 156px;
+    height: 56px;
+    margin: 0 10px;
+`;
+
+export const ButtonText = styled.Text`
+    margin: auto;
+    font-family: 'Inter-Medium-sInt=0';
+    text-align: center;
+    font-size: 16px;
+    color: #e1e1e6;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+    width: 156px;
+    height: 40px;
+    margin: 40px auto 0;
+`;
+
+export const BackButtonText = styled.Text`
+    margin: auto;
+    font-family: 'Archivo-SemiBold';
+    /*text-align: center;*/
+    font-size: 15px;
+    color: #7a7a80;
 `;
