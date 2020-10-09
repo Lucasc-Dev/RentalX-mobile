7a071c728ae31d0e4c5a6d6ccba3d4faaced40b2
Lucas César <lucasambrogi1@hotmail.com>
2020-10-08 23:29:02 -0300

    Input component created.

--- src/pages/Start/CreateAccount/index.tsx
+++ src/pages/Start/CreateAccount/index.tsx
@@ -2,17 +2,30 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/Feather'
 
+//import Input from '../../../components/Input';
+
 import { 
   Container,
-  HorizontalContainer, 
+  TextContainer,
+  Title,
+  Subtitle,
+  ButtonsContainer,
 } from './styles';
 
 const CreateAccount: React.FC = () => {
   return (
     <Container>
-      <HorizontalContainer>
-        <Icon name="chevron-left" />
-      </HorizontalContainer>
+      <Icon size={20} name="chevron-left" color="#AEAEB3" />
+
+      <TextContainer>
+        <Title>Estamos quase lá.</Title>
+        
+        <Subtitle>Faça seu login para começar uma experiência incrível.</Subtitle>
+      </TextContainer>
+
+      <ButtonsContainer>
+        {/* <Input /> */}
+      </ButtonsContainer>
     </Container>
   );
 };
--- src/pages/Start/CreateAccount/styles.ts
+++ src/pages/Start/CreateAccount/styles.ts
@@ -7,3 +7,27 @@ export const Container = styled.View`
 export const HorizontalContainer = styled.View`
 
 `;
+
+export const TextContainer = styled.View`
+    width: 220px;
+`;
+
+export const Title = styled.Text`
+    font-family: 'Archivo-SemiBold';
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 43px;
+    color: #3d3d4d;
+`;
+
+export const Subtitle = styled.Text`
+    font-family: 'Inter-Medium-sInt=0';
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 25px;
+    color: #7a7a80;
+`;
+
+export const ButtonsContainer = styled.View`
+
+`;
