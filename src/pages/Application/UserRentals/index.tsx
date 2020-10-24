b7911c5cfc865deb7c6e6730cecc2dd79bdd33b7
Lucas César <lucasambrogi1@hotmail.com>
2020-10-23 23:56:52 -0300

    UserRentals page: JSX and styles done.

--- src/pages/Application/UserRentals/index.tsx
+++ src/pages/Application/UserRentals/index.tsx
@@ -1,11 +1,160 @@
 import React from 'react';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { 
+  Container,
+  Header,
+  HeaderTitleContainer,
+  Title,
+  TotalVehicles,
+  VehiclesContainer,
+  Vehicle,
+  VehicleContainer,
+  VehicleInfoContainer,
+  TextContainer,
+  VehicleSubtitle,
+  VehicleTitle,
+  VehiclePrice,
+  VehicleImage,
+  PeriodContainer,
+  DateContainer,
+  PeriodText,
+} from './styles';
 
 const UserRentals: React.FC = () => {
   return (
     <Container>
+      <Header>
+        <HeaderTitleContainer>
+          <Title>Agendamentos</Title>
+
+          <TotalVehicles>12 carros</TotalVehicles>
+        </HeaderTitleContainer>
+      </Header>
+
+      <VehiclesContainer>
+        <Vehicle>
+          <VehicleContainer>
+            <VehicleInfoContainer>
+              <TextContainer>
+                <VehicleSubtitle>Lamborghini</VehicleSubtitle>
+                <VehicleTitle>Huracan</VehicleTitle>
+              </TextContainer>
+              <TextContainer>
+                <VehicleSubtitle>Ao dia</VehicleSubtitle>
+                <VehiclePrice>R$ 120</VehiclePrice>
+              </TextContainer>
+            </VehicleInfoContainer>
+
+            <Icon name="droplet" size={20} color="#aeaeb3" style={{ alignSelf: 'flex-end' }} />
+
+            <VehicleImage source={{ uri: 'https://somarautomoveis.com/wp-content/uploads/2019/11/carro-png-destaque.png' }} />
+          </VehicleContainer>
+          <PeriodContainer>
+            <VehicleSubtitle>Período</VehicleSubtitle>
+
+            <DateContainer>
+              <PeriodText>17 Junho 2019</PeriodText>
+
+              <Icon name="arrow-right" size={20} color="#aeaeb3" />
+
+              <PeriodText>22 Junho 2020</PeriodText>
+            </DateContainer>
+          </PeriodContainer>
+        </Vehicle>
 
 
+        <Vehicle>
+          <VehicleContainer>
+            <VehicleInfoContainer>
+              <TextContainer>
+                <VehicleSubtitle>Lamborghini</VehicleSubtitle>
+                <VehicleTitle>Huracan</VehicleTitle>
+              </TextContainer>
+              <TextContainer>
+                <VehicleSubtitle>Ao dia</VehicleSubtitle>
+                <VehiclePrice>R$ 120</VehiclePrice>
+              </TextContainer>
+            </VehicleInfoContainer>
+
+            <Icon name="droplet" size={20} color="#aeaeb3" style={{ alignSelf: 'flex-end' }} />
+
+            <VehicleImage source={{ uri: 'https://somarautomoveis.com/wp-content/uploads/2019/11/carro-png-destaque.png' }} />
+          </VehicleContainer>
+          <PeriodContainer>
+            <VehicleSubtitle>Período</VehicleSubtitle>
+
+            <DateContainer>
+              <PeriodText>17 Junho 2019</PeriodText>
+
+              <Icon name="arrow-right" size={20} color="#aeaeb3" />
+
+              <PeriodText>22 Junho 2020</PeriodText>
+            </DateContainer>
+          </PeriodContainer>
+        </Vehicle>
+
+
+        <Vehicle>
+          <VehicleContainer>
+            <VehicleInfoContainer>
+              <TextContainer>
+                <VehicleSubtitle>Lamborghini</VehicleSubtitle>
+                <VehicleTitle>Huracan</VehicleTitle>
+              </TextContainer>
+              <TextContainer>
+                <VehicleSubtitle>Ao dia</VehicleSubtitle>
+                <VehiclePrice>R$ 120</VehiclePrice>
+              </TextContainer>
+            </VehicleInfoContainer>
+
+            <Icon name="droplet" size={20} color="#aeaeb3" style={{ alignSelf: 'flex-end' }} />
+
+            <VehicleImage source={{ uri: 'https://somarautomoveis.com/wp-content/uploads/2019/11/carro-png-destaque.png' }} />
+          </VehicleContainer>
+          <PeriodContainer>
+            <VehicleSubtitle>Período</VehicleSubtitle>
+
+            <DateContainer>
+              <PeriodText>17 Junho 2019</PeriodText>
+
+              <Icon name="arrow-right" size={20} color="#aeaeb3" />
+
+              <PeriodText>22 Junho 2020</PeriodText>
+            </DateContainer>
+          </PeriodContainer>
+        </Vehicle>
+        
+        <Vehicle>
+          <VehicleContainer>
+            <VehicleInfoContainer>
+              <TextContainer>
+                <VehicleSubtitle>Lamborghini</VehicleSubtitle>
+                <VehicleTitle>Huracan</VehicleTitle>
+              </TextContainer>
+              <TextContainer>
+                <VehicleSubtitle>Ao dia</VehicleSubtitle>
+                <VehiclePrice>R$ 120</VehiclePrice>
+              </TextContainer>
+            </VehicleInfoContainer>
+
+            <Icon name="droplet" size={20} color="#aeaeb3" style={{ alignSelf: 'flex-end' }} />
+
+            <VehicleImage source={{ uri: 'https://somarautomoveis.com/wp-content/uploads/2019/11/carro-png-destaque.png' }} />
+          </VehicleContainer>
+          <PeriodContainer>
+            <VehicleSubtitle>Período</VehicleSubtitle>
+
+            <DateContainer>
+              <PeriodText>17 Junho 2019</PeriodText>
+
+              <Icon name="arrow-right" size={20} color="#aeaeb3" />
+
+              <PeriodText>22 Junho 2020</PeriodText>
+            </DateContainer>
+          </PeriodContainer>
+        </Vehicle>
+      </VehiclesContainer>
     </Container>
   );
 };
--- src/pages/Application/UserRentals/styles.ts
+++ src/pages/Application/UserRentals/styles.ts
@@ -1,5 +1,100 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+    flex: 1;
+    background-color: #FFF;
+`;
 
+export const Header = styled.View`
+    width: 100%;
+    
+    background-color: #1b1b1f;
+`;
+
+export const HeaderTitleContainer = styled.View`
+    margin: 24px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Title = styled.Text`
+    font-family: 'Archivo-SemiBold';
+    font-size: 25px;
+    color: #FFF;
+`;
+
+export const TotalVehicles = styled.Text`
+    font-family: 'Inter-Regular';
+    font-size: 13px;
+    color: #7a7a80;
+`;
+
+export const VehiclesContainer = styled.ScrollView``;
+
+export const Vehicle = styled.View`
+    margin: 16px 16px 0;
+`;
+
+export const VehicleContainer = styled.View`
+    padding: 24px;
+    background-color: #f4f5f6;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const VehicleInfoContainer = styled.View`
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export const TextContainer = styled.View`
+
+`;
+
+export const VehicleSubtitle = styled.Text`
+    font-family: 'Archivo-Medium';
+    text-transform: uppercase;
+    font-size: 10px;
+    color: #aeaeb3;
+`;
+
+export const VehicleTitle = styled.Text`
+    font-family: 'Archivo-Medium';
+    font-size: 15px;
+    color: #47474d;
+`;
+
+export const VehiclePrice = styled.Text`
+    font-family: 'Archivo-Medium';
+    font-size: 15px;
+    color: #dc1637;
+`;
+
+export const VehicleImage = styled.Image.attrs({ resizeMode: 'contain' })`
+    margin-left: 16px;
+    width: 170px;
+    height: 90px;
+`;
+
+export const PeriodContainer = styled.View`
+    margin-top: 2px;
+    padding: 15px 24px;
+    background-color: #f4f5f6;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const DateContainer = styled.View`
+    width: 220px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const PeriodText = styled.Text`
+    font-family: 'Inter-Regular';
+    font-size: 13px;
+    color: #47474D;
 `;
