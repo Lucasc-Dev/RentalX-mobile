42fff5d6fdab00911f613bdb907d9201c312ad1d
Lucas César <lucasambrogi1@hotmail.com>
2020-10-24 22:14:41 -0300

    VehicleDetails page: created.

---
+++ src/pages/Application/VehicleDetails/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const VehicleDetails: React.FC = () => {
+  return (
+    <Container>
+      <Text>VehicleDetails</Text>
+    </Container>
+  );
+};
+
+export default VehicleDetails;
