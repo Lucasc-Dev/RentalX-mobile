34508810d774d73a35f6ad0c75253e2d1563e2f6
Lucas César <lucasambrogi1@hotmail.com>
2020-10-26 23:51:11 -0300

    SuccessfulRental page: started.

---
+++ src/pages/Application/SuccessfulRental/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SuccessfulRental: React.FC = () => {
+  return (
+    <Container>
+      <Text>SuccessfulRental</Text>
+    </Container>
+  );
+};
+
+export default SuccessfulRental;
