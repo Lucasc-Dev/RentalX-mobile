9893629f08061c79296b67e09edabd8696776244
Lucas César <lucasambrogi1@hotmail.com>
2020-10-27 23:12:48 -0300

    SuccessfulRental page: JSX done.

--- src/pages/Application/SuccessfulRental/index.tsx
+++ src/pages/Application/SuccessfulRental/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-import { Container } from './styles';
+import backgroundImage from '../../../assets/images/Union.png';
+import doneIcon from '../../../assets/icons/Done.png';
+
+import { 
+  Container,
+  ImageBackground,
+  DoneImage,
+  TitleContainer,
+  Title,
+  Subtitle,
+  Button,
+  ButtonText,
+} from './styles';
 
 const SuccessfulRental: React.FC = () => {
   return (
     <Container>
-      <Text>SuccessfulRental</Text>
+      <ImageBackground source={backgroundImage} resizeMode="cover" />
+      <DoneImage source={doneIcon} />
+      <TitleContainer>
+        <Title>Carro alugado!</Title>
+
+        <Subtitle>Agora você só precisa ir até a concessionária da RentalX para pegar o seu automóvel.</Subtitle>
+      </TitleContainer>
+
+      <Button>
+        <ButtonText>Pronto</ButtonText>
+      </Button>
     </Container>
   );
 };
