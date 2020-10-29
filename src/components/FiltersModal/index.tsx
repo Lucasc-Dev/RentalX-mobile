519d1ec0947c1af1d6f736dc4150cf5ec9312271
Lucas César <lucasambrogi1@hotmail.com>
2020-10-28 23:52:52 -0300

    FiltersModal component: JSX done.

--- src/components/FiltersModal/index.tsx
+++ src/components/FiltersModal/index.tsx
@@ -1,11 +1,85 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Button from '../Button';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  HeaderSubtitle,
+  FilterField,
+  FilterTitleContainer,
+  FilterTitle,
+  RangeSelect,
+  FuelContainer,
+  Fuel,
+  FuelIcon,
+  FuelDescription,
+  GearContainer,
+  Gear,
+  GearDescription,
+} from './styles';
 
 const FiltersModal: React.FC = () => {
   return (
     <Container>
+      <Header>
+        <HeaderTitle>Filtro</HeaderTitle>
 
+        <HeaderSubtitle>Limpar filtros</HeaderSubtitle>
+      </Header>
+
+      <FilterField>
+        <FilterTitleContainer>
+          <FilterTitle>Preço ao dia</FilterTitle>
+        </FilterTitleContainer>
+
+        <RangeSelect>
+
+        </RangeSelect>
+      </FilterField>
+
+      <FilterField>
+        <FilterTitle>Combustível</FilterTitle>
+
+        <FuelContainer>
+          <Fuel>
+            <FuelIcon source={{ uri: '' }} />
+
+            <FuelDescription>Gasolina</FuelDescription>
+          </Fuel>
+
+          <Fuel>
+            <FuelIcon source={{ uri: '' }} />
+
+            <FuelDescription>Elétrico</FuelDescription>
+          </Fuel>
+
+          <Fuel>
+            <FuelIcon source={{ uri: '' }} />
+
+            <FuelDescription>Flex</FuelDescription>
+          </Fuel>
+        </FuelContainer>
+      </FilterField>
+
+      <FilterField>
+        <FilterTitle>Câmbio</FilterTitle>
+
+        <GearContainer>
+          <Gear>
+            <GearDescription>Automático</GearDescription>
+          </Gear>
+
+          <Gear>
+            <GearDescription>Manual</GearDescription>
+          </Gear>
+        </GearContainer>
+      </FilterField>
+
+      <Button 
+        text="Confirmar"
+      />
     </Container>
   );
 };
--- src/components/FiltersModal/styles.ts
+++ src/components/FiltersModal/styles.ts
@@ -3,3 +3,59 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
 
 `;
+
+export const Header = styled.View`
+
+`;
+
+export const HeaderTitle = styled.Text`
+
+`;
+
+export const HeaderSubtitle = styled.Text`
+
+`;
+
+export const FilterField = styled.View`
+
+`;
+
+export const FilterTitleContainer = styled.View`
+
+`;
+
+export const FilterTitle = styled.Text`
+
+`;
+
+export const RangeSelect = styled.View`
+
+`;
+
+export const FuelContainer = styled.View`
+
+`;
+
+export const Fuel = styled.View`
+
+`;
+
+export const FuelIcon = styled.Image`
+
+`;
+
+export const FuelDescription = styled.Text`
+
+`;
+
+export const GearContainer = styled.View`
+
+`;
+
+export const Gear = styled.View`
+
+`;
+
+export const GearDescription = styled.Text`
+
+`;
