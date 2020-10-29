e3c83e4fe76219aa55748d54b0bab456585ff607
Lucas César <lucasambrogi1@hotmail.com>
2020-10-28 22:30:40 -0300

    FiltersModal component: started.

---
+++ src/components/FiltersModal/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const FiltersModal: React.FC = () => {
+  return (
+    <Container>
+
+    </Container>
+  );
+};
+
+export default FiltersModal;
