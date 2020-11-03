c149ca2b70da260fb090bd8f148b1b25e997e7c6
Lucas César <lucasambrogi1@hotmail.com>
2020-11-03 19:10:08 -0300

    Input component: uncore implemented.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import Icon from 'react-native-vector-icons/Feather';
 
@@ -15,7 +16,32 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
+interface InputValueReference {
+  value: string;
+}
+
 const Input: React.FC<InputProps> = ({ icon, name, ...rest }) => {
+  const inputElementRef = useRef<any>(null);
+
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value: string) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      }
+      });
+  }, []);
+
   return (
     <Container>
       <IconBox>
@@ -24,8 +50,10 @@ const Input: React.FC<InputProps> = ({ icon, name, ...rest }) => {
 
       <InputBox>
         <TextInput 
+          ref={inputElementRef}
           keyboardAppearance="light"
           placeholderTextColor="#666360" 
+          onChangeText={(value) => { inputValueRef.current.value = value }}
           {...rest} 
         />
       </InputBox>
