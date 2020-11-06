ace061dd095cbefcf968ec839b20eae63505c344
Lucas César <lucasambrogi1@hotmail.com>
2020-11-05 23:50:12 -0300

    EditProfileNameEmail page: created.

---
+++ src/pages/Application/EditProfilePages/EditProfileNameEmail/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const EditNameEmailProfile: React.FC = () => {
+  return (
+    <Container>
+      <Text>EditNameEmailProfile</Text>
+    </Container>
+  );
+};
+
+export default EditNameEmailProfile;
