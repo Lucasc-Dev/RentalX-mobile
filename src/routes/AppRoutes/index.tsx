dcc1bcd2d4ac09404b8cc8174c4217a2eea29b33
Lucas César <lucasambrogi1@hotmail.com>
2020-11-12 21:37:09 -0300

    Routes: added EditProfile and EditProfileSuccessful pages.

--- src/routes/AppRoutes/index.tsx
+++ src/routes/AppRoutes/index.tsx
@@ -7,6 +7,8 @@ import TabPagesStack from './TabPagesStack';
 import SelectPeriod from '../../pages/Application/SelectPeriod';
 import VehicleDetails from '../../pages/Application/VehicleDetails';
 import SuccessfulRental from '../../pages/Application/SuccessfulRental';
+import EditProfile from '../../pages/Application/EditProfile';
+import EditProfileSuccessful from '../../pages/Application/EditProfileSuccessful';
 
 const App = createStackNavigator();
 
@@ -19,11 +21,11 @@ const AuthRoutes: React.FC = () => {
             }}
         >
             <App.Screen name="SelectPeriod" component={SelectPeriod} />
-
             <App.Screen name="TabPagesStack" component={TabPagesStack} />
-
             <App.Screen name="VehicleDetails" component={VehicleDetails} />
             <App.Screen name="SuccessfulRental" component={SuccessfulRental} />
+            <App.Screen name="EditProfile" component={EditProfile} />
+            <App.Screen name="EditProfileSuccessful" component={EditProfileSuccessful} />
         </App.Navigator>
     );
 }
