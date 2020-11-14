871ac8b9e8e4499f6032ea9ee8556e1ac09c5ba6
Lucas César <lucasambrogi1@hotmail.com>
2020-11-13 23:55:12 -0300

    SignIn page: added validations to the submit form function.

--- src/pages/Start/SingIn/index.tsx
+++ src/pages/Start/SingIn/index.tsx
@@ -5,6 +5,9 @@ import { useAuth } from '../../../hooks/AuthContext';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import * as Yup from 'yup';
+import getValidationErrors from '../../../utils/getValidationErrors';
+
 import Icon from 'react-native-vector-icons/Feather'
 
 import Input from '../../../components/Input';
@@ -40,6 +43,15 @@ const SignIn: React.FC = () => {
 
   const handleSubmitButton = useCallback(async (data: SubmitFormData) => {
     try{
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+
       await signIn({
         email: data.email,
         password: data.password,
@@ -47,7 +59,18 @@ const SignIn: React.FC = () => {
       
       navigate('SelectPeriod');
     }catch (err) {
-      Alert.alert('Houve um problema ao fazer login.');
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Erro na autenticação', 
+        'Ocorreu um erro ao fazer login, cheque as credenciais.'
+      );
     }
   }, [signIn, navigate, user]);
 
@@ -66,9 +89,11 @@ const SignIn: React.FC = () => {
         <Form ref={formRef} onSubmit={ handleSubmitButton }>
           <ButtonsContainer>
             <Input
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
               name="email"
               placeholder="E-mail"
-              autoCorrect={false}
               icon="mail"
               returnKeyType="next"
               onSubmitEditing={() => {
