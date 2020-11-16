83ea21a0c704bafb43e389075a4a62ee52cf9264
Lucas César <lucasambrogi1@hotmail.com>
2020-11-15 23:52:45 -0300

    Custom icon and splash screen: added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
-      <StatusBar />
+      <StatusBar backgroundColor="#1b1b1f" />
       <AppProvider>
         <View style={{ flex: 1, backgroundColor: '#E1E1E6' }}>
           <Routes />
