7bc72d20bc52188556b2bdd94f5030a76c08ffc2
Lucas César <lucasambrogi1@hotmail.com>
2020-12-03 15:55:17 -0300

    Axios config: updated api url.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.100:3333',
+    baseURL: 'https://api.rentalx.lucascesar.com.br',
 });
 
 export default api;
